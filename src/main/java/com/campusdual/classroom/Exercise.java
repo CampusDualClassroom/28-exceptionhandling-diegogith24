21390aad32f4bed5cb4c4ebd9d141c0f02614458
Diego Armando Castillo Montoya <castillodiego2401@gmail.com>
2025-11-05 08:24:22 +0100

    Solved Exercise 28

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,7 +1,20 @@
 package com.campusdual.classroom;
 
+import java.io.IOException;
+
 public class Exercise {
     public static void main(String[] args) {
+        try {
+            System.out.println("Intentando una operación que puede fallar.");
+
+            throw new IOException("Simulación de error de entrada/salida: no se pudo leer el archivo.");
+
+        }
+
+        catch (IOException e) {
+            System.err.println("\n*** Excepción Controlada ***");
+
+        }
 
     }
 }
